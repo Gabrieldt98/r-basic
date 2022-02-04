1e408d747209e582c9ace3959f20e2b4e0044f49
Gabriel Dávila <gabrieldt98@gmail.com>
2022-02-03 23:38:56 -0600

    Práctica 03/02/2022

---
+++ scripts/MisScripts/04-calculadora.R
@@ -0,0 +1,106 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3+5
+
+2%%3
+
+2^3*5
+
+2^-4
+
+725%/%7
+725%%7
+# Se cumple algoritmo de Euclides 
+# D (dividendo) = d (divisor) * q (cociente) + r (resto)
+# r = D - d * q
+# q = D%/%d
+# x (r) = D %% d
+
+725-7*103
+
+pi
+
+2*pi
+
+3^pi
+
+pi^2
+
+NA
+NaN
+5/0
+0/0
+
+
+factorial(4)
+
+choose(5, 3)
+
+x = pi^2/2
+
+
+
+
+Hola = 5
+HOLA = 4
+nombre = "Gabriel Alejandro"
+# 5pi
+pi.5 = 5*pi
+x = 2
+x = x^2
+x = sqrt(x)
+
+edad = 23
+
+e = 23%/%2
+r = 23%%2
+
+## Función f(x) = x^3 - (3^x)*sen(x)
+
+f = function(x){
+  x^3-(3^x)*sin(x)
+  }
+f(4) # 4^3 - 3^4 * sin(4)
+f(5)
+f(pi/2)
+
+
+suma1 <- function(t){
+  t+1
+}
+suma1(6)
+suma1(-5)
+
+product <- function(x,y){
+  x*y
+}
+
+product(3,5)
+product(7,4)
+
+g <- function(x,y,z){
+  exp(x^2 + y^2) * sin(z)
+}
+g(1,2,3)
+g(1,-1,pi)
+
+
+suma5 <- function(numero){
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  suma1(numero)
+}
+
+suma5(3)
+
+ls()
+rm(product)
+ls()
+rm(list = ls())
+ls()
